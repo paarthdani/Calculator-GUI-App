f0dd29f4a3f7ffa7fe835dbc8a0cc177c4074cd3
Parth Dani <parth_dani@yahoo.in>
2016-10-04 14:14:50 +0530

    Update GUI Calculator.py

--- GUI Calculator.py
+++ GUI Calculator.py
@@ -4,64 +4,60 @@ from tkinter import ttk
 
 class Calculator:
 
-    # Stores the current value to display in the entry
+    
     calc_value = 0.0
 
-    # Will define if this was the last math button clicked
+    
     div_trigger = False
     mult_trigger = False
     add_trigger = False
     sub_trigger = False
     ac_trigger = False
 
-    # Called anytime a number button is pressed
+    
     def button_press(self, value):
 
-        # Get the current value in the entry
+        
         entry_val = self.number_entry.get()
 
-        # Put the new value to the right of it
-        # If it was 1 and 2 is pressed it is now 12
-        # Otherwise the new number goes on the left
+        
         entry_val += value
 
-        # Clear the entry box
+        
         self.number_entry.delete(0, "end")
 
-        # Insert the new value going from left to right
+        
         self.number_entry.insert(0, entry_val)
 
-    # Returns True or False if the string is a float
+    
     def isfloat(self, str_val):
         try:
 
-            # If the string isn't a float float() will throw a
-            # ValueError
+            
             float(str_val)
 
-            # If there is a value you want to return use return
+            
             return True
         except ValueError:
             return False
 
-    # Handles logic when math buttons are pressed
+    
     def math_button_press(self, value):
 
-        # Only do anything if entry currently contains a number
+        
         if self.isfloat(str(self.number_entry.get())):
 
-            # make false to cancel out previous math button click
+            
             self.add_trigger = False
             self.sub_trigger = False
             self.mult_trigger = False
             self.div_trigger = False
             self.ac_trigger = False
 
-            # Get the value out of the entry box for the calculation
+            
             self.calc_value = float(self.entry_value.get())
 
-            # Set the math button click so when equals is clicked
-            # that function knows what calculation to use
+            
             if value == "/":
                 print("/ Pressed")
                 self.div_trigger = True
@@ -78,16 +74,13 @@ class Calculator:
                 print("AC Pressed")
                 self.ac_trigger = True
 
-            # Clear the entry box
+            
             self.number_entry.delete(0, "end")
 
-    # Performs a mathematical operation by taking the value before
-    # the math button is clicked and the current value. Then perform
-    # the right calculation by checking what math button was clicked
-    # last
+    
     def equal_button_press(self):
 
-        # Make sure a math button was clicked
+        
         if self.add_trigger or self.sub_trigger or self.mult_trigger or self.div_trigger:
 
             if self.add_trigger:
@@ -111,19 +104,19 @@ class Calculator:
 
 
     def __init__(self, root):
-        # Will hold the changing value stored in the entry
+        
         self.entry_value = StringVar(root, value="")
 
-        # Define title for the app
+        
         root.title("Calculator")
 
-        # Defines the width and height of the window
+        
         root.geometry("600x240")
 
-        # Block resizing of Window
+        
         root.resizable(width=False, height=False)
 
-        # Customize the styling for the buttons and entry
+        
         style = ttk.Style()
         style.configure("TButton",
                         font="Serif 15",
@@ -133,7 +126,7 @@ class Calculator:
                         font="Serif 18",
                         padding=10)
 
-        # Create the text entry box
+        
         self.number_entry = ttk.Entry(root,
                         textvariable=self.entry_value, width=50)
         self.number_entry.grid(row=0, columnspan=4)
@@ -178,11 +171,11 @@ class Calculator:
 
         self.button_sub = ttk.Button(root, text="-", command=lambda: self.math_button_press('-')).grid(row=4, column=3)
 
-# Get the root window object
+
 root = Tk()
 
-# Create the calculator
+
 calc = Calculator(root)
 
-# Run the app until exited
+
 root.mainloop()
